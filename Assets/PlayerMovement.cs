464b506d5de2682bd7a9c0504c72eb0a180a2301
kromduiwel <su.opperman@gmail.com>
2025-12-17 14:44:25 +0200

    Player Movement update

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal"); // A/D or Left/Right
+        movement.y = Input.GetAxisRaw("Vertical");   // W/S or Up/Down
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
